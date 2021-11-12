ea355ee6db38e1474d56b5d40a5d187af75fce40
LakkaGopiChandu <93304527+LakkaGopiChandu@users.noreply.github.com>
2021-11-12 11:38:40 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hi to all hope all are doing good,  happy weekend to all and be healthy and wealthy")
